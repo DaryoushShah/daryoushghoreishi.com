aa8c2c89f804ababa0cafa6c2f33fe9cca7868c1
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2023-04-11 12:50:56 -0700

    Add footer and About

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*==================== typed js ====================*/
 const typed = new Typed('.multiple-text', {
-    strings: ['Analyst', 'Software Developer', 'Analyst'],
+    strings: ['Analyst', 'Software Developer', 'Leader'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
